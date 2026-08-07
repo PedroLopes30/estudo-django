52eec69709ac226e6f36f0abc7de8dbc86c4960a
Pedro Lopes <pedrohenriquelds1506@gmail.com>
2026-08-07 11:26:17 -0300

    feat(recipes): refactor templates and styles for improved structure and functionality

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 # Create your views here.
 
 def home (request):
     return render(request,'recipes/pages/home.html', {
-        'name': 'Pedro Lopes'
+        'recipes': [make_recipe() for _ in range(20)]
     })
 
 def recipe (request, id):
     return render(request,'recipes/pages/recipe-view.html', {
-        'name': 'Pedro Lopes'
-    })
+        'recipe': make_recipe(),
+        'is_detail_page': True
+    })  
