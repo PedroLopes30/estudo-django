7273b06d3c563e687a2dad39acb799755bf473c8
Pedro Lopes <pedrohenriquelds1506@gmail.com>
2026-08-07 20:08:01 -0300

    feat(recipes): implement recipe and category models, views, and templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from utils.recipes.factory import make_recipe
-# Create your views here.
+from django.http import Http404
+from .models import Recipe
 
 def home (request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request,'recipes/pages/home.html', {
-        'recipes': [make_recipe() for _ in range(20)]
+        'recipes': recipes,
+    })
+
+def category (request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not found :(')
+    
+    return render(request,'recipes/pages/category.html', {
+        'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | '
     })
 
 def recipe (request, id):
+    recipe = get_object_or_404(Recipe, pk=id)
+
     return render(request,'recipes/pages/recipe-view.html', {
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True
     })  
